5a95da06525cecba76cb4b6623be7034b84056bc
unknown <nano.software.systems@gmail.com>
2022-06-27 12:30:45 +0200

    I was modified index.js and index.html. Was added React to index.js and div they are has the div with id="app"

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello vscode');
+const Paragraph = () => {
+
+  return (
+    <p className="paragraph" id="first-paragraph">Lorem ipsum dolor sit amet</p>
+  )
+
+}
+
+const appContainer = document.querySelector('#app')
+
+ReactDOM.render(<Paragraph />, appContainer)
